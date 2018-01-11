8a8b312bd64577bfa88ea1092be651f96c4da8f5
dev_dinhpv <phamdoanmanu38@gmail.com>
2018-01-11 09:59:10 +0700

    Initial commit

---
+++ app/src/main/java/vn/viettel/corepermission/FragmentPermissionActivity.java
@@ -0,0 +1,13 @@
+package vn.viettel.corepermission;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FragmentPermissionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment_permission);
+    }
+}
